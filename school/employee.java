1bc4552d15cba0efa5a3c77a223ba52dabe84d9e
Giftmuli <giftmuli6742@mail.com>
2026-02-16 10:35:17 +0300

    added emplpyee class

--- school/employee.java
+++ school/employee.java
@@ -1,5 +1,26 @@
 package school;
+import java.util.Scanner;
+public class employee extends person{
+    String staffno;
+    String department;
 
-public class employee {
-    
+    void addEmployee()
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("EMPLOYEE MANAGEMENT");
+        System.out.println("Enter fullname:");
+        fullname=input.nextLine();
+        System.out.println("Enter gender:");
+        gender=input.nextLine();
+        System.out.println("Enter staff number:");
+        staffno=input.nextLine();
+        System.out.println("Enter department:");
+        department=input.nextLine();
+
+        //output variables
+        System.out.println("Fullname: "+fullname);
+        System.out.println("Gender: "+gender);
+        System.out.println("Staff number: "+staffno);
+        System.out.println("Department: "+department);
+    }
 }
